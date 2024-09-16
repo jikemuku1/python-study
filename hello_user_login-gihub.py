2dbfa1ac2fa348560f1b0c05ac7a8095b09d0cc9
jikemuku1 <168145270+jikemuku1@users.noreply.github.com>
2024-09-16 01:59:28 -0400

    Add files via upload
    Introduction to Python: interactive mode, script mode,
    operators, variables, types, input and output.

---
+++ hello_user_login-gihub.py
@@ -0,0 +1,11 @@
+"""
+Author: 
+"""
+def main():
+    
+    user_input = input("Enter your name:")
+    output=f"{user_input} Hello {user_input}!"
+    print(output)
+"""Do not change anything below this line."""
+if __name__ == "__main__":
+    main()
